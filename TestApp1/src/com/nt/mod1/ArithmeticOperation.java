708d05451c92e61548eed5b499d4dca4429b6c42
rajeshDev2 <Dev2@g0mail>
2017-04-30 04:58:24 -0700

    dev2-change4

---
+++ TestApp1/src/com/nt/mod1/ArithmeticOperation.java
@@ -0,0 +1,10 @@
+package com.nt.mod1;
+
+public class ArithmeticOperation {
+
+	
+	public int sum(int x , int y) {
+		return x+y;
+	}
+
+}
--- TestApp1/src/com/nt/test/SampleApp.java
+++ TestApp1/src/com/nt/test/SampleApp.java
@@ -1,5 +1,7 @@
 package com.nt.test;
 
+import com.nt.mod1.ArithmeticOperation;
+
 public class SampleApp {
 public static void main(String[] args) {
 	System.out.println("sample app1");
@@ -10,6 +12,9 @@ public static void main(String[] args) {
 	System.out.println("sample app3--->Hello");
 	System.out.println("sample app5");
 	System.out.println("sample app4");
+	
+	ArithmeticOperation aop = new ArithmeticOperation();
+	System.out.println(aop.sum(10, 20));
 }
 
 }
